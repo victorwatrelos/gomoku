f9668f53b9e74b66cf29521e953c11708df9612e
Joseph RENOUF-GAY <jrenouf-@student.42.fr>
2016-09-28 20:18:11 +0200

    wip

---
+++ heuristics/SimpleHeuristic.cpp
@@ -0,0 +1,21 @@
+#include "SimpleHeuristic.hpp"
+
+SimpleHeuristic::SimpleHeuristic(void) {}
+
+SimpleHeuristic::SimpleHeuristic(const SimpleHeuristic & rhs)
+{
+	*this = rhs;
+}
+
+SimpleHeuristic::~SimpleHeuristic(void) {}
+
+SimpleHeuristic&		SimpleHeuristic::operator=(const SimpleHeuristic & rhs)
+{
+	(void)rhs;
+	return *this;
+}
+
+int					SimpleHeuristic::eval(Board *b)
+{
+	return b->getScore();
+}
