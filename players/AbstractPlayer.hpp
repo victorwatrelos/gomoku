20ceeb369ac6b879c7f850e2cdb8a07c9a8263f7
Watrelos Victor <victor.watrelos@gmail.com>
2016-10-28 12:28:32 +0200

    Disp info

--- players/AbstractPlayer.hpp
+++ players/AbstractPlayer.hpp
@@ -14,17 +14,18 @@ class AbstractPlayer
 		AbstractPlayer &operator=(const AbstractPlayer &p);
 		virtual ~AbstractPlayer(void);
 		virtual int			getMove(const Board & board) = 0;
+		virtual std::string	getInfo(void) const;
 		const std::string	&getName(void) const;
 		const Board::Point	&getColor(void) const;
 		void				addTime(long long t);
-		void				dispAverage(void);
-		std::string			getAverage(void);
-		std::string			getLastTime(void);
+		void				dispAverage(void) const;
+		std::string			getAverage(void) const;
+		std::string			getLastTime(void) const;
 	protected:
 		std::string		_name;
 		Board::Point	_color;
 	private:
-		std::string		_getStrTime(long long);
+		std::string		_getStrTime(long long) const;
 		int				_totGetMove = 0;
 		long long		_totTime = 0;
 		std::vector<long long>	_times;
