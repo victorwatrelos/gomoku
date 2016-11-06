018e93fc412a4bca8ab1aff93956baa0f38eb9d8
Victor WATRELOS-GUILBERT <victor.watrelos@gmail.com>
2016-11-06 19:18:10 +0100

    WIP

--- utilities/LineData.cpp
+++ utilities/LineData.cpp
@@ -9,7 +9,8 @@ LineData::~LineData(void)
 
 static int		my_pow(int n)
 {
-	/*
+	if (n > 5)
+		n = 5;
 	switch (n)
 	{
 		case 0:
@@ -29,10 +30,6 @@ static int		my_pow(int n)
 		default:
 			return std::pow(4, n);
 	};
-	*/
-	if (n > 5)
-		n = 5;
-	return std::pow(4, n);
 }
 
 int						LineData::_getStoneScore(void)
